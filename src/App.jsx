1455cf994ae0bdf1ccef2aaeb67cd17785a79d11
eoakpinarr <eoakpinar@icloud.com>
2024-10-17 06:12:11 +0300

    değiştir

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ import PrivateRoute from './PrivateRoute';
 const App = () => {
   return (
     <div className='flex flex-col min-h-screen'>
-      <Router basename="/eoakpinar.com.tr">
+      <Router basename="/">
         <ScrollToTop />
         <Navbar />
         <ContactMe />
