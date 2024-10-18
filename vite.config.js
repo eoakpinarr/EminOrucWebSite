275d489744cac0d2006dc7d3847b8738dd0ad576
Etem Oğulcan AKPINAR <eoakpinar@icloud.com>
2024-10-18 23:58:15 +0300

    sitemap oluşturuldu. robots.txt oluşturuldu

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import sitemap from 'vite-plugin-sitemap';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://eoakpinar.com.tr', // Sitenizin ana URL'si
+      // Opsiyonel: Ekstra yollar veya sayfalar ekleyebilirsiniz
+      routes: [
+        '/about', 
+        '/hizmetlerimiz', 
+        '/blog', 
+        '/contact', 
+        '/hizmetlerimiz/ceza-hukuku', 
+        '/hizmetlerimiz/aile-hukuku', 
+        '/hizmetlerimiz/miras-hukuku',
+        '/hizmetlerimiz/gayrimenkul-hukuku',
+        '/hizmetlerimiz/ticaret-hukuku',
+        '/hizmetlerimiz/i̇dare-hukuku',
+        '/hizmetlerimiz/kira-hukuku',
+        '/hizmetlerimiz/i̇ş-hukuku',
+        '/hizmetlerimiz/i̇cra-hukuku',
+        '/hizmetlerimiz/araç-değer-kaybı'
+      ], // Elle eklemek istediğiniz yollar
+    }),
+  ],
   base: '/' // GitHub Pages için
 });
