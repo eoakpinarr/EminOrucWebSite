a50a22d028cb35a21c4c8b1256a2440a655d9499
Etem Oğulcan AKPINAR <eoakpinar@icloud.com>
2024-10-22 19:39:02 +0300

    Slider görselleri düzenlendi webp

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,44 +1,49 @@
 // src/components/ImageSlider.js
 import React from 'react';
-import './Slider.css'
+import './Slider.css';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Autoplay } from 'swiper/modules';
 
-import Image1 from '../../assets/images/SlideImages/1-1.png'
-import Image3 from '../../assets/images/SlideImages/3.jpg'
-
+import Image1 from '../../assets/images/SlideImages/1-1.webp';
+import Image2 from '../../assets/images/SlideImages/3.webp';
+import Image3 from '../../assets/images/SlideImages/1-1.webp';
+import Image4 from '../../assets/images/SlideImages/3.webp';
 
 const Slider = () => {
-    const images = [
-        Image1,
-        Image3,
-    ];
-
     return (
         <div className="slider-container 2xl:mb-40 mb-10"> {/* Slider container ile sarıyoruz */}
             <Swiper
+                slidesPerView={1} // Sadece bir slayt göster
+                slidesPerGroup={1} // Gruplama için 1
                 modules={[Navigation, Autoplay]}
                 navigation
-                loop={true}
-                slidesPerView={1} // veya ihtiyaca göre ayarla
-                slidesPerGroup={1} // veya ihtiyaca göre ayarla
-                speed={3000}
-                autoplay={{
-                    delay: 1000,
-                    disableOnInteraction: false,
-                }}
+                loop={true} // Döngü modu
+                speed={3000} // Geçiş hızı
+                autoplay={{ delay: 1000 }} // Otomatik kaydırma
             >
-                {images.map((image, index) => (
-                    <SwiperSlide key={index}>
-                        <img src={image} alt={`Slide ${index + 1}`} />
-                    </SwiperSlide>
-                ))}
+                <SwiperSlide>
+                    <img src={Image1} alt="Avukat Muhammed Emin ORUÇ" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={Image2} alt="ORUÇ Hukuk & Danışmanlık" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={Image3} alt="Avukat Muhammed Emin ORUÇ" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={Image4} alt="ORUÇ Hukuk & Danışmanlık" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={Image3} alt="Avukat Muhammed Emin ORUÇ" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={Image4} alt="ORUÇ Hukuk & Danışmanlık" />
+                </SwiperSlide>
             </Swiper>
         </div>
-
     );
 };
 
