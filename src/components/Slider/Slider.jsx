cc81af2968452c1140379dd2a1be67ea4754bebe
Etem Oğulcan AKPINAR <eoakpinar@icloud.com>
2024-10-23 12:34:57 +0300

    index

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './Slider.css';
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -9,16 +9,6 @@ import Image1 from '../../assets/images/SlideImages/1.webp';
 import Image2 from '../../assets/images/SlideImages/3-2.webp';
 
 const Slider = () => {
-    useEffect(() => {
-        const preloadImages = () => {
-            const img1 = new Image();
-            const img2 = new Image();
-            img1.src = Image1;
-            img2.src = Image2;
-        };
-
-        preloadImages();
-    }, []);
 
     return (
         <div className="slider-container mb-10">
@@ -34,7 +24,7 @@ const Slider = () => {
                     <img src={Image1} alt="Avukat Muhammed Emin ORUÇ" />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src={Image2} alt="ORUÇ Hukuk & Danışmanlık" loading="lazy"/>
+                    <img src={Image2} alt="ORUÇ Hukuk & Danışmanlık" />
                 </SwiperSlide>
             </Swiper>
         </div>
